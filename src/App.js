84334150a682c45d8e078982e35ff78a5e959ebf
Priyanka <original.manaria@gmail.com>
2022-02-04 11:30:46 +0530

    finished this project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
@@ -7,6 +7,24 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 4000);
+
+    return () => clearInterval(slider);
+  }, [index]);
 
   return (
     <section className="section">
@@ -25,12 +43,16 @@ function App() {
           }
           if (
             personIndex === index - 1 ||
-            personIndex === people.length - 1
+            (index === 0 && personIndex === people.length - 1)
           ) {
             position = "lastSlide";
           }
           return (
-            <article className={position} key={id}>
+            <article
+              style={{ transition: "all 1s ease" }}
+              className={position}
+              key={id}
+            >
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
